48a4f0622677b69b07003a1ba6f14c9a64ec94ae
Atik Hasan <atiktrail108@gmail.com>
2026-01-21 02:20:38 +0600

    erro fix

--- auth.py
+++ auth.py
@@ -1,16 +1,18 @@
 from google_auth_oauthlib.flow import Flow
 from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
+# --- CHANGED: "installed" -> "web" ---
+# This matches the new credentials you just created.
 CLIENT_CONFIG = {
-    "installed": {
+    "web": {
         "client_id": CLIENT_ID,
         "client_secret": CLIENT_SECRET,
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs"
     }
 }
 
-# Scopes: What permissions we are asking for
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
     "https://www.googleapis.com/auth/userinfo.email",
@@ -19,10 +21,6 @@ SCOPES = [
 ]
 
 def get_flow(state=None):
-    """
-    Creates a Google OAuth Flow object.
-    'state' is the secret ID we pass to Google to remember WHICH user is logging in.
-    """
     flow = Flow.from_client_config(
         CLIENT_CONFIG, 
         scopes=SCOPES, 
